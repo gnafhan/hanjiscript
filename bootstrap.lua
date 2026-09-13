3bbf128d337a4e065dfbbc41124cb84f5c9b81b7
gnafhan <nafhanghifari@gmail.com>
2026-09-14 00:35:18 +0700

    chore: pin bootstrap to path overlay release

--- bootstrap.lua
+++ bootstrap.lua
@@ -1,4 +1,4 @@
-local BASE_URL = "https://raw.githubusercontent.com/gnafhan/hanjiscript/ef9049a"
+local BASE_URL = "https://raw.githubusercontent.com/gnafhan/hanjiscript/0c1c4af"
 
 local function getFetch()
 	if type(http_request) == "function" then
