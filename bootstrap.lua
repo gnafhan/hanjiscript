66a51adeb93cab9fc6e37d52dbcea5834f045241
gnafhan <nafhanghifari@gmail.com>
2026-09-13 23:25:06 +0700

    chore: pin bootstrap to planner target fix

--- bootstrap.lua
+++ bootstrap.lua
@@ -1,4 +1,4 @@
-local BASE_URL = "https://raw.githubusercontent.com/gnafhan/hanjiscript/363d83f"
+local BASE_URL = "https://raw.githubusercontent.com/gnafhan/hanjiscript/886a22e"
 
 local function getFetch()
 	if type(http_request) == "function" then
