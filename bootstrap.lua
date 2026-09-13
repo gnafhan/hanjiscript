8e1fbf65fc32dd8e4334e0154c829940856339c6
gnafhan <nafhanghifari@gmail.com>
2026-09-13 23:32:33 +0700

    chore: pin bootstrap to telemetry release

--- bootstrap.lua
+++ bootstrap.lua
@@ -1,4 +1,4 @@
-local BASE_URL = "https://raw.githubusercontent.com/gnafhan/hanjiscript/f0a55bd"
+local BASE_URL = "https://raw.githubusercontent.com/gnafhan/hanjiscript/4a3b6ee"
 
 local function getFetch()
 	if type(http_request) == "function" then
