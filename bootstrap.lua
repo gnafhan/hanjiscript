aa15a6c5bc73ec097e069fb1d5d1f21501beb6ea
gnafhan <nafhanghifari@gmail.com>
2026-09-13 21:40:09 +0700

    chore: pin bootstrap BASE_URL to dedupe fix 69a6b6b

--- bootstrap.lua
+++ bootstrap.lua
@@ -1,4 +1,4 @@
-local BASE_URL = "https://raw.githubusercontent.com/gnafhan/hanjiscript/73408988cc5a435371889ac3d41646e5462b024b"
+local BASE_URL = "https://raw.githubusercontent.com/gnafhan/hanjiscript/69a6b6b6ef4539bc38b3d2a0bdefb38f13b29491"
 
 local function getFetch()
 	if type(http_request) == "function" then
